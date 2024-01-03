13b21dd2bb0b0305ac621b8bab05f13e1fdabed2
igdmitrov <id.personal.info@gmail.com>
2024-01-03 09:09:13 +0200

    Changed logic for preview

---
+++ QuizBot/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace QuizBot;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<Worker>();
+            })
+            .Build();
+
+        host.Run();
+    }
+}
